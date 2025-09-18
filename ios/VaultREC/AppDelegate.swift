72ac510520bbf348433874e55a4068ecc262a76b
Odgerel Zolboo <zolboo@Odgerels-MacBook-Pro.local>
2025-09-18 22:14:47 +0800

    change name

--- ios/VaultREC/AppDelegate.swift
+++ ios/VaultREC/AppDelegate.swift
@@ -28,7 +28,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "Vault",
+      withModuleName: "Vault REC",
       in: window,
       launchOptions: launchOptions
     )
